89c95b61af6d53b8b4d8e9ab7ac8eec57a97ebfd
Micah Jackson <gmicahjackson@gmail.com>
2024-01-31 16:13:42 -0800

    storage tested, url gen tested
    should be 2 commits
    
    - commit 1: storage service added. Can init connection to Redis db, save and retrieve url information
    
    - commit 2: url generation service added. Hash using sha256, encode to text using base58 (opposed to base64), take shortened url string as first 8 characters of text encoding

--- main.go
+++ main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	loadEnvironmentVariables()
+
+	router := gin.Default()
+	mapRoutes(router)
+	serveAPI(router)
+}
+
+func loadEnvironmentVariables() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func mapRoutes(router *gin.Engine) {
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hey Go URL Shortener !",
 		})
 	})
+}
 
-	err := r.Run(":9808")
+func serveAPI(router *gin.Engine) {
+	err := router.Run(":9808")
 	if err != nil {
 		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
 	}
---
+++ shortener/shorturl_generator.go
@@ -0,0 +1,37 @@
+package shortener
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/itchyny/base58-go"
+)
+
+func sha256Of(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write([]byte(input))
+	return algorithm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
+}
+
+// Algorithm to generate short link
+// - Hashing  initialUrl + userId url with sha256.  Here userId is added to prevent providing similar shortened urls to separate users in case they want to shorten exact same link, its a design decision, so some implementations do this differently.
+// - Derive a big integer number from the hash bytes generated during the hasing.
+// - Finally apply base58  on the derived big integer value and pick the first 8 characters
+func GenerateShortLink(initialLink string, userId string) string {
+	urlHashBytes := sha256Of(initialLink + userId)
+	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
+	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
+	return finalString[:8]
+}
